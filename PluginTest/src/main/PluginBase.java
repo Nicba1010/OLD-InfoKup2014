3755db0e00f84ff268bbc29ad7077fec076bb557
Roberto Anić Banić <nicba1010@gmail.com>
2014-01-30 21:26:07 +0100

    PluginSystem first commit

---
+++ PluginTest/src/main/PluginBase.java
@@ -0,0 +1,7 @@
+package main;
+
+public interface PluginBase {
+
+	public void run();
+
+}
---
+++ PluginTest/src/pljugin/Plugin.java
@@ -0,0 +1,15 @@
+package pljugin;
+
+import main.PluginBase;
+
+public class Plugin implements PluginBase {
+
+	public Plugin() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void run() {
+		System.out.println("PERIŠA JE IDIOT");
+	}
+
+}
