9e544bcf75222b428b325e6afbfa92bebc1866c6
Labos <ucenik@lb1-ws1.cobjekt1.tsrb.edu>
2014-01-31 11:14:38 +0100

    Novi plugin

---
+++ PluginTest/src/plugin2/Plugin.java
@@ -0,0 +1,24 @@
+package plugin2;
+
+import javax.swing.JPanel;
+
+import main.PluginBase;
+
+public class Plugin implements PluginBase {
+
+	public Plugin() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		System.out.println("jA SAM plugin");
+	}
+
+	@Override
+	public void addJButtons(JPanel panel) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
